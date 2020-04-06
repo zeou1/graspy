9486d770fcd687f58e8ba5459717f0e7546471c1
Anton Alyakin <alyakin314@gmail.com>
2020-04-06 19:36:57 +0300

    rewrote some of the seedless procrustes tests

--- tests/test_seedless_procrustes.py
+++ tests/test_seedless_procrustes.py
@@ -10,54 +10,45 @@ from graspy.align import SeedlessProcrustes
 
 class TestSeedlessProcrustes(unittest.TestCase):
     def test_bad_kwargs(self):
-        with self.asertRaises(TypeError):
+        with self.assertRaises(TypeError):
             SeedlessProcrustes(lambda_init="oops")
-        with self.asertRaises(TypeError):
+        with self.assertRaises(TypeError):
             SeedlessProcrustes(lambda_final="oops")
-        with self.asertRaises(TypeError):
+        with self.assertRaises(TypeError):
             SeedlessProcrustes(alpha="oops")
-        with self.asertRaises(TypeError):
+        with self.assertRaises(TypeError):
             SeedlessProcrustes(optimal_transport_eps="oops")
-        with self.asertRaises(TypeError):
+        with self.assertRaises(TypeError):
             SeedlessProcrustes(iterative_eps="oops")
-        with self.asertRaises(TypeError):
+        with self.assertRaises(TypeError):
             SeedlessProcrustes(num_reps=3.15)
-        with self.asertRaises(ValueError):
-            SeedlessProcrustes(alpha=-1)
-        with self.asertRaises(ValueError):
-            SeedlessProcrustes(optimal_transport_eps=-1)
-        with self.asertRaises(ValueError):
-            SeedlessProcrustes(iterative_eps=-1)
-        with self.asertRaises(ValueError):
+        with self.assertRaises(ValueError):
+            SeedlessProcrustes(alpha=-0.01)
+        with self.assertRaises(ValueError):
+            SeedlessProcrustes(optimal_transport_eps=-0.01)
+        with self.assertRaises(ValueError):
+            SeedlessProcrustes(iterative_eps=-0.01)
+        with self.assertRaises(ValueError):
             SeedlessProcrustes(num_reps=0)
 
     def test_sign_flips(self):
         X = np.arange(6).reshape(3, 2)
         Y = np.arange(6).reshape(3, 2) @ np.diag([1, -1]) + 0.5
-        test = _sign_flips(X, Y) 
+
+        aligner = SeedlessProcrustes()
+        test = aligner._sign_flips(X, Y) 
         answer = np.array([[1, 0], [0, -1]])
         self.assertTrue(np.all(test == answer))
 
-    def test_iterative_optimal_transport(self):
-        np.random.seed(314)
-        X = np.random.normal(1, 0.2, 1000).reshape(250, 4)
-        Y = np.concatenate([X, X])
-        W = stats.ortho_group.rvs(4)
-        Y = Y @ W
-        ​
-        aligner = SeedlessProcrustes()
-        _, Q = aligner._iterative_optimal_transport(X, Y)
-        self.assertTrue(np.all(np.isclose(Q, W)))
-
     def test_matching_datasets(self):
-        np.random.seed(31412)
-        X = np.random.normal(0.2, 0.02, 1000).reshape(200, 5)
+        np.random.seed(314)
+        X = np.random.normal(1, 0.2, 1000).reshape(-1, 4)
         Y = np.concatenate([X, X])
         W = stats.ortho_group.rvs(4)
         Y = Y @ W
-        ​
+
         aligner = SeedlessProcrustes()
-        Q = aligner.fit_transform(X, Y)
+        Q = aligner.fit_predict(X, Y, Q=np.eye(4))
         self.assertTrue(np.all(np.isclose(Q, W)))
 
 if __name__ == "__main__":
