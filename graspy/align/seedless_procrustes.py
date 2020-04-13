93ddcc200d0c96ff8d8f46e4785764b29941b584
Anton Alyakin <alyakin314@gmail.com>
2020-04-13 11:04:04 +0300

    introduced a capability to perform 2d restarts

--- graspy/align/seedless_procrustes.py
+++ graspy/align/seedless_procrustes.py
@@ -46,6 +46,24 @@ class SeedlessProcrustes:
 
         num_reps : int, optional
             the number of reps for each subiteration
+
+    initiazlization: string, {"2d", "sign_flips", "custom"}
+        - "2d"
+            uses 2^d different initiazlizations, where d is the dimension.
+            specifically, uses all possible matrices with all entries real and
+            diagonal entries having magnitude 1 and 0s everywehre else. for
+            example, for d=2, tries [[1, 0], [0, 1]], [[1, 0], [0, -1]],
+            [[-1, 0], [0, 1]], and [[-1, 0], [0, -1]]. picks the best one based
+            on the value of the objective function.
+        - 'sign_flips'
+            for the first dimension, if two datasets have medians with varying
+            signs, flips all signs along this dimension for the first dataset.
+            then initializes to an identity.
+        - "custom"
+            expects either an initial matrix Q or initial matrix P during the
+            use of fit or fit_transform. uses initial Q provided, unless it is
+            not provided. if not provided - uses initial P. if neither is given
+            initializes to Q = I.
             
     Attributes
     ----------
@@ -68,6 +86,7 @@ class SeedlessProcrustes:
         optimal_transport_eps=0.01,
         iterative_eps=0.01,
         num_reps=100,
+        initialization="2d",
     ):
         if type(lambda_init) is not float:
             raise TypeError()
@@ -82,35 +101,44 @@ class SeedlessProcrustes:
         if type(num_reps) is not int:
             raise TypeError()
         if alpha < 0 or alpha > 1:
-            raise ValueError(
-                "{} is an invalid value of alpha must be strictly between 0 and 1".format(
-                    alpha
-                )
+            msg = "{} is an invalid value of alpha must be strictly between 0 and 1".format(
+                alpha
             )
+            raise ValueError(msg)
         if optimal_transport_eps <= 0:
-            raise ValueError(
-                "{} is an invalud value of the optimal transport eps, must be postitive".format(
-                    optimal_transport_eps
-                )
+            msg = "{} is an invalud value of the optimal transport eps, must be postitive".format(
+                optimal_transport_eps
             )
+            raise ValueError(msg)
         if iterative_eps <= 0:
-            raise ValueError(
-                "{} is an invalud value of the iterative eps, must be postitive".format(
-                    iterative_eps
-                )
+            msg = "{} is an invalud value of the iterative eps, must be postitive".format(
+                iterative_eps
             )
+            raise ValueError(msg)
         if num_reps < 1:
-            raise ValueError(
-                "{} is invalid number of repetitions, must be greater than 1".format(
-                    num_reps
-                )
+            msg = "{} is invalid number of repetitions, must be greater than 1".format(
+                num_reps
             )
+            raise ValueError(msg)
+
+        if not isinstance(initialization, str):
+            msg = "initialization must be None or a str, not {}".format(
+                type(initialization)
+            )
+            raise TypeError(msg)
+        else:
+            initializations_supported = ["2d", "sign_flips", "custom"]
+            if initialization not in initializations_supported:
+                msg = "supported initializations are {}".format(initialization)
+                raise NotImplementedError(msg)
+
         self.lambda_init = lambda_init
         self.lambda_final = lambda_final
         self.alpha = alpha
         self.optimal_transport_eps = optimal_transport_eps
         self.iterative_eps = iterative_eps
         self.num_reps = num_reps
+        self.initialization = initialization
 
     def _procrustes(self, X, Y, P_i):
         u, w, vt = np.linalg.svd(X.T @ P_i @ Y)
@@ -155,6 +183,18 @@ class SeedlessProcrustes:
         t = (val > 0) * 2 - 1
         return np.diag(t)
 
+    def orthogonal_matrix_from_int(self, val_int, d):
+        val_bin = bin(val_int)[2:]
+        val_bin = "0" * (d - len(val_bin)) + val_bin
+        return np.diag(np.array([(float(i) - 0.5) * -2 for i in val_bin]))
+
+    def match_datasets(self, X, Y, Q):
+        lambda_current = self.lambda_init
+        while lambda_current > self.lambda_final:
+            P_i, Q = self._iterative_optimal_transport(X, Y, Q, lambd=lambda_current)
+            lambda_current = self.alpha * lambda_current
+        return P_i, Q
+
     def fit(self, X, Y, Q=None, P=None):
         """
         matches the datasets
@@ -181,19 +221,35 @@ class SeedlessProcrustes:
         self: returns an instance of self
         """
         # TODO check bad matrix inputs
-        if Q is None:
-            if P is None:
-                Q = self._sign_flips(X, Y)
-                # Q = np.eye(X.shape[1])
+        # make sure dimensions are the same
+        if self.initialization == "2d":
+            n, d = X.shape
+            m, _ = Y.shape
+            P_matrices = np.zeros((2 ** d, n, m))
+            Q_matrices = np.zeros((2 ** d, d, d))
+            objectives = np.zeros(2 ** d)
+            for i in range(2 ** d):
+                initial_Q = self.orthogonal_matrix_from_int(i, d)
+                P_matrices[i], Q_matrices[i] = P, Q = self.match_datasets(
+                    X, Y, initial_Q
+                )
+                objectives[i] = np.linalg.norm(X @ Q - P @ Y, ord="fro")
+            best = np.argmin(objectives)
+            self.initial_Q = self.orthogonal_matrix_from_int(best, d)
+            self.P, self.Q = P_matrices[best], Q_matrices[best]
+        elif self.initialization == "sign_flips":
+            self.initial_Q = self._sign_flips(X, Y)
+            self.P, self.Q = self.match_datasets(X, Y, self.initial_Q_)
+        else:
+            if Q is None:
+                if P is None:
+                    self.initial_Q = np.eye(X.shape[1])
+                else:
+                    self.initial_Q = self._procrustes(X, Y, P)
             else:
-                Q = self._procrustes(X, Y, P)
+                self.initial_Q = Q
+            self.P, self.Q = self.match_datasets(X, Y, self.initial_Q)
 
-        lambda_current = self.lambda_init
-        while lambda_current > self.lambda_final:
-            P_i, Q = self._iterative_optimal_transport(X, Y, Q, lambd=lambda_current)
-            lambda_current = self.alpha * lambda_current
-        self.Q = Q
-        self.P = P_i
         return self
 
     def fit_predict(self, X, Y, Q=None, P=None):
